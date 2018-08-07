ad815aa476478945155506062c5e0fd480ecb3d3
Lynne Daniels <lynne33477@gmail.com>
2018-08-07 08:04:36 -0700

    fix(image): fix error when image properties are missing from data

--- src/Book.js
+++ src/Book.js
@@ -20,12 +20,13 @@ class Book extends React.Component {
   render() {
 
     const { book } = this.props;
-    const placeholderImg = '/icons/placeholderImg.svg';
+    console.log(book);
+    const placeholderImg = './icons/placeholderImg.svg';
     return (
       <div>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail?book.imageLinks.thumbnail:placeholderImg})` }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks?book.imageLinks.thumbnail:placeholderImg})` }}></div>
             <div className="book-shelf-changer">
               <select value={this.state.value} onChange={(e) => this.handleChange(e)}>
                 <option value="move" disabled>Move to...</option>
